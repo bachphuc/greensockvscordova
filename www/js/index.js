8c6c6166033781e99db7718f4904c4bebf2e6bca
Bach Phuc <trieuquangphuc123@gmail.com>
2015-07-19 23:38:43 +0700

    home

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -26,25 +26,71 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', app.onDeviceReady, false);
+        if (typeof cordova === 'undefined') {
+            $(document).ready(app.onDocumentReady);
+        }
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log('Device ready...');
+        app.onDocumentReady();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onDocumentReady: function() {
+        console.log('Document ready...');
+        app.leftMenu = document.getElementById('left-menu');
+        app.backdrop = document.getElementById('backdrop');
+        app.bShowMenu = true;
+        app.toggleMenuButton = $('#toggle-menu');
+        app.toggleMenuButton.on('click', function() {
+            app.toggleMenu();
+        });
+    },
+    hideMenu: function() {
+        if (!this.leftMenu) {
+            return;
+        }
+        var newWidth = (this.leftMenu.offsetWidth * -1);
+        TweenMax.to(this.backdrop, this.menuSpeed, {
+            opacity: 0
+        });
+        TweenMax.to(this.leftMenu, this.menuSpeed, {
+            // left: newWidth + "px",
+            x: newWidth,
+            // ease: Power2.easeInOut,
+            // opacity: 0.4,
+            autoRound: false
+        });
+        this.bShowMenu = false;
+    },
+    showMenu: function() {
+        if (!this.leftMenu) {
+            return;
+        }
+        TweenMax.to(this.backdrop, this.menuSpeed, {
+            opacity: 0.6
+        });
+        TweenMax.to(this.leftMenu, this.menuSpeed, {
+            // left: "0px",
+            x: 0,
+            // ease: Power2.easeInOut,
+            // opacity: 1,
+            autoRound: false
+        });
+        this.bShowMenu = true;
+    },
+    menuSpeed: 0.5,
+    toggleMenu: function() {
+        if (!this.leftMenu) {
+            return;
+        }
+        if (this.bShowMenu) {
+            return app.hideMenu();
+        }
+        app.showMenu();
     }
 };
 
